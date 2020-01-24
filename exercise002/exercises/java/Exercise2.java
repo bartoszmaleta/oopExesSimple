f01d70306ef73c7bffcc323775f322204b0177eb
Bartosz Maleta <bartosz.maleta@gmail.com>
2020-01-24 14:05:39 +0100

    Add exe2

---
+++ exercise002/exercises/java/Exercise2.java
@@ -0,0 +1,11 @@
+package exercise002.exercises.java;
+
+public class Exercise2 {
+    public static void main(String[] args) {
+        int num1 = 4;
+        int num2 = 6;
+        int result = num1 + num2;
+
+        System.out.println(result);
+    }
+}
