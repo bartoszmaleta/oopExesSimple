c2bca78e54327d6c454ed9e34755bf3a9e4c5884
Bartosz Maleta <bartosz.maleta@gmail.com>
2020-01-24 14:03:43 +0100

    Add exe1

---
+++ exercise001/exercises/java/Exercise1.java
@@ -0,0 +1,8 @@
+// package exercises.java;
+package exercise001.exercises.java;
+
+public class Exercise1 {
+    public static void main(String[] args) {
+        System.out.println("Exercise1 executed");
+    }
+}
